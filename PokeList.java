397102b5b00ce6601d7ba79908b97a9087a630ea
Amir <iskandar.amir@gmail.com>
2018-12-02 00:28:23 +0800

    Add files via upload

---
+++ PokeList.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author amir
+ */
+public class PokeList {
+
+    public ArrayList<String> getList() {
+
+        ArrayList<String> pList = new ArrayList<>();
+
+        InputStream IS = getClass().getResourceAsStream("Pokemons.txt");
+        //try (Scanner is = new Scanner(new FileInputStream("Pokemons/" + name + ".txt"))) {
+        try (Scanner is = new Scanner(IS)) {
+            //read pokemon
+            while (is.hasNext()) {
+                if (is.nextLine().contains("$")) {
+                    pList.add(is.nextLine());
+                }
+            }
+        }        
+        
+        return pList;
+    }
+}
