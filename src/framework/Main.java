0c28c6e92c8a7a28edb65f63aeae17abca68cd53
nrlfdh <45528945+nrlfdh@users.noreply.github.com>
2018-12-06 10:21:57 +0800

    AH ambek kau

--- src/framework/Main.java
+++ src/framework/Main.java
@@ -3,48 +3,6 @@ package framework;/*
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-import scenes.SceneHandler;
-
-import java.io.IOException;
-
-/**
- * @author amir
- */
-
-
-public class Main extends Application {
-
-    public static Stage window;
-    public static boolean inCombat = false;
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    public static void setInCombat(boolean inCombat) {
-        Main.inCombat = inCombat;
-    }
-
-    public void start(Stage primaryStage) {
-        window = primaryStage;
-        window.setOnCloseRequest(e -> {
-            e.consume();
-            exitProgram();
-        });
-
-        window.setScene(SceneHandler.menu());
-        window.setTitle("Pokémon X");
-
-
-        window.show();
-
-    }
-
-
     private static void exitProgram() {
         if (popups.ConfirmBox.display("Exit?", "Are you sure you want to exit?")) {
 
