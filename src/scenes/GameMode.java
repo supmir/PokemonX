3c24cc678725d56dc2ce3c38c37b799cd3566375
nrlfdh <fadhlianazaini@gmail.com>
2018-12-11 00:45:10 +0800

    approve pls

--- src/scenes/GameMode.java
+++ src/scenes/GameMode.java
@@ -1,20 +1,29 @@
 package scenes;
 
 import framework.Main;
+import javafx.scene.image.ImageView;
 import tools.Writer;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import tools.getres.GetResource;
 
 class GameMode {
 
     public static Scene start() {
+        ImageView imageHolder = new ImageView();
+
+        GetResource get = new GetResource();
+        imageHolder.setImage(get.getImage("backgd"));
+
+
         Label border = new Label();
         Label txt = new Label("Choose your game mode :");
         border.setMinSize(400, 50);
         border.setBorder(Main.styles.getBorder());
+        border.setStyle("-fx-background-color: white;");
         StackPane stack = new StackPane();
         stack.getChildren().addAll(border, txt);
 
@@ -43,8 +52,10 @@ class GameMode {
         layout1.getChildren().addAll(stack, PvP, PvC, back);
         layout1.setAlignment(Pos.CENTER);
 
+        StackPane all = new StackPane();
+        all.getChildren().addAll(imageHolder, layout1);
 
-        return new Scene(layout1, 800, 800);
+        return new Scene(all, 800, 800);
 
     }
 }
